df907d0f82ff6cd658f46ac1504f3768c336f8ab
mikey-esteban <michaelp.esteban@gmail.com>
2022-09-27 09:28:55 -0400

    changed component from functional to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { BpkCode } from 'bpk-component-code';
 import BpkButton from 'bpk-component-button';
 import BpkText from 'bpk-component-text';
@@ -7,6 +7,7 @@ import format from 'date-fns/format';
 
 import STYLES from './App.scss';
 
+const c = className => STYLES[className] || 'UNKNOWN';
 const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
 const formatMonth = date => format(date, 'MMMM yyyy');
 const daysOfWeek = [
@@ -16,42 +17,93 @@ const daysOfWeek = [
     index: 0,
     isWeekend: true,
   },
-  // ...
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tues',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thur',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    index: 6,
+    isWeekend: true,
+  },
 ];
 
-const c = className => STYLES[className] || 'UNKNOWN';
+export default class App extends Component {
 
+  constructor () {
+    super();
 
-const App = () => {
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      }
+    };
+  }
 
+  handleDateSelect = (date) => {
+    this.setState({
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: date,
+      },
+    });
+  }
 
-  return (
-    <div className={c('App')}>
-      <header className={c('App__header')}>
-        <div className={c('App__header-inner')}>
-          <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Flight Schedule</BpkText>
-        </div>
-      </header>
-      <main className={c('App__main')}>
-        <BpkCalendar
-          id='calendar'
-          // onDateSelect={handleDateSelect}
-          formatMonth={formatMonth}
-          formatDateFull={formatDateFull}
-          daysOfWeek={daysOfWeek}
-          weekStartsOn={1}
-          changeMonthLabel="Change month"
-          nextMonthLabel="Next month"
-          previousMonthLabel="Previous month"
-          // selectionConfiguration={selectionConfiguration}
-        />
-        <BpkText tagName="p" className={c('App__text')}>
-          To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-        </BpkText>
-        <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
-      </main>
-    </div>
-  )
+  render () {
+    return (
+      <div className={c('App')}>
+        <header className={c('App__header')}>
+          <div className={c('App__header-inner')}>
+            <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Flight Schedule</BpkText>
+          </div>
+        </header>
+        <main className={c('App__main')}>
+          <BpkCalendar
+            id='calendar'
+            onDateSelect={handleDateSelect}
+            formatMonth={formatMonth}
+            formatDateFull={formatDateFull}
+            daysOfWeek={daysOfWeek}
+            weekStartsOn={1}
+            changeMonthLabel="Change month"
+            nextMonthLabel="Next month"
+            previousMonthLabel="Previous month"
+            selectionConfiguration={selectionConfiguration}
+          />
+          <BpkText tagName="p" className={c('App__text')}>
+            To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
+          </BpkText>
+          <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+        </main>
+      </div>
+    )
+  }
 };
-
-export default App;
